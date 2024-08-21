35d59bb11ed5a983a412a73279cc51d8b9babcb9
BrunaMICHELEbelo975 <brunabelo975@gmail.com>
2024-08-21 20:48:10 -0300

    Criçao do Projeto Java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
+
+     Integer idadePessoa = 20;
+     String nomePessoa = "faffezinho";
+
+     /*
+     * operadores logicos
+     * > maior
+     * >= maior igual
+     * < menor
+     * <= menor igual
+     * == é igual
+     * != é diferente
+     * equal compara textos
+      */
+
+     if(idadePessoa > 21){
+         System.out.println("é maior que 21");
+     }else {
+         System.out.println("não é maior");
+     }}}
